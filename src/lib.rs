7f9430317bca140087ee845c762960d4c558cf6e
Charles Earp <charles.r.earp@gmail.com>
2019-05-22 08:38:19 -0700

    Working test code for opencl kernel.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-
-/*
-use std::ops::Deref;
+use std::ops::{Deref, Add, Sub, Mul, Div};
+use ocl;
 
 pub type Ix1 = usize;
 pub type Ix2 = (usize, usize);
 
-pub trait Shape {
+pub trait Shape: Copy + Eq + std::fmt::Debug {
   fn rmaj_strides(&self) -> Self;
   fn product(&self) -> usize;
  
@@ -31,9 +30,132 @@ impl Shape for Ix2 {
 
 #[derive(Default, Debug)]
 pub struct Tensor<S, D> {
-  shape: S,
-  strides: S,
-  data: D
-}*/
+  pub shape: S,
+  pub strides: S,
+  pub data: D
+}
+
+pub type TensorOwned<S, T> = Tensor<S, Vec<T>>;
+pub type TensorView<'a, S, T> = Tensor<S, &'a [T]>; 
+
+impl<S, T> TensorOwned<S, T>
+  where S: Shape {
+  pub fn shape_uninit(shape: S) -> Self {
+    let strides = shape.rmaj_strides();
+    let n = shape.product();
+    let mut data = Vec::with_capacity(n);
+    unsafe { data.set_len(n) };
+    Self{shape, strides, data}
+  }
+  pub fn shape_elem(shape: S, elem: T) -> Self
+    where T: Clone {
+    let strides = shape.rmaj_strides();
+    let n = shape.product();
+    let mut data = Vec::with_capacity(n);
+    data.resize(n, elem); 
+    Self{shape, strides, data}
+  }
+  pub fn view<'a>(&'a self) -> TensorView<'a, S, T> {
+    TensorView{shape: self.shape,
+               strides: self.strides,
+               data: self.data.deref()}
+  } 
+}   
+
+impl<S, D, T> Deref for Tensor<S, D>
+  where D: Deref<Target=[T]> {
+  type Target = [T];
+  fn deref(&self) -> &[T] {
+    &*self.data
+  }
+}  
+
+pub trait CLType {
+  fn name() -> &'static str;
+} 
+
+impl CLType for f32 {
+  fn name() -> &'static str { &"float" }
+}
+
+impl CLType for f64 {
+  fn name() -> &'static str { &"double" }
+}
+
+impl CLType for i32 {
+  fn name() -> &'static str { &"int" }
+}
+
+macro_rules! impl_tensor_op {
+  ($op_trait:ident, $func:ident, $op_str:expr) => {
+    impl<S, D1, D2, T> $op_trait<Tensor<S, D2>> for Tensor<S, D1>
+      where S: Shape,
+            D1: Deref<Target=[T]>,
+            D2: Deref<Target=[T]>,
+            T: Clone + $op_trait<T, Output=T> + ocl::OclPrm + CLType {
+      type Output = TensorOwned<S, T>;
+      #[inline]
+      fn $func(self, rhs: Tensor<S, D2>) -> Self::Output {
+        debug_assert_eq!(self.shape, rhs.shape); 
+        let mut out = TensorOwned::shape_uninit(self.shape);
+        let src = format!(
+          "__kernel void add(__global {} const* const a, __global {} const* const b, __global {}* const c) {{
+            int idx = get_global_id(0);
+            c[idx] = a[idx] {} b[idx];
+          }}",
+          T::name(),
+          T::name(),
+          T::name(),
+          $op_str
+        );  
+        let pro_que = ocl::ProQue::builder()
+          .src(src)
+          .dims(self.shape.product())
+          .build()
+          .unwrap();
+        let a = unsafe {
+          pro_que.buffer_builder()
+                 .flags(ocl::MemFlags::new().read_only())
+                 .use_host_slice(self.deref())
+                 .build()
+                 .unwrap()
+        }; 
+        let b = unsafe {
+          pro_que.buffer_builder()
+                 .flags(ocl::MemFlags::new().read_only())
+                 .use_host_slice(rhs.deref())
+                 .build()
+                 .unwrap()
+        }; 
+        let c = unsafe {
+          pro_que.buffer_builder()
+                 .flags(ocl::MemFlags::new().write_only())
+                 .use_host_slice(rhs.deref())
+                 .build()
+                 .unwrap()
+        }; 
+        let kernel = pro_que.kernel_builder("add")
+          .arg(&a)
+          .arg(&b)
+          .arg(&c)
+          .build()
+          .unwrap();
+        unsafe { 
+          kernel.enq()
+                .unwrap(); 
+        }
+        c.read(&mut out.data)
+         .enq()
+         .unwrap();
+        out
+      }
+    }
+  }
+}
+
+impl_tensor_op!(Add, add, "+");
+impl_tensor_op!(Sub, sub, "-");
+impl_tensor_op!(Mul, mul, "*");
+impl_tensor_op!(Div, div, "/");
 
     
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,24 @@
-use rspirv;
-use spirv_headers as spirv;
-
-use rspirv::binary::Assemble;
-use rspirv::binary::Disassemble;
+use adtensor::{Tensor, TensorOwned};
+use timeit::{timeit, timeit_loops};
 
 fn main() {
-  // Building
-  let mut b = rspirv::mr::Builder::new();
-  b.memory_model(spirv::AddressingModel::Logical, spirv::MemoryModel::GLSL450);
-  let void = b.type_void();
-  let voidf = b.type_function(void, vec![void]);
-  b.begin_function(void,
-                   None,
-                   spirv::FunctionControl::DONT_INLINE |
-                    spirv::FunctionControl::CONST,
-                   voidf)
-   .unwrap();
-  b.begin_basic_block(None).unwrap();
-  b.ret().unwrap();
-  b.end_function().unwrap();
-  let module = b.module();
-
-  // Assembling
-  let code = module.assemble();
-  assert!(code.len() > 20);  // Module header contains 5 words
-  assert_eq!(spirv::MAGIC_NUMBER, code[0]);
-
-  // Parsing
-  let mut loader = rspirv::mr::Loader::new();
-  rspirv::binary::parse_words(&code, &mut loader).unwrap();
-  let module = loader.module();
-  println!("{:?}", module);
-  println!("{}", module.disassemble());
-  /*
-  // Disassembling
-  assert_eq!(module.disassemble(),
-             "; SPIR-V\n\
-              ; Version: 1.3\n\
-              ; Generator: rspirv\n\
-              ; Bound: 5\n\
-              OpMemoryModel Logical GLSL450\n\
-              %1 = OpTypeVoid\n\
-              %2 = OpTypeFunction %1 %1\n\
-              %3 = OpFunction  %1  DontInline|Const %2\n\
-              %4 = OpLabel\n\
-              OpReturn\n\
-              OpFunctionEnd");*/
+  let n = 10000000;
+  let x = Tensor::shape_elem(n, 1f32);
+  let y = Tensor::shape_elem(n, 2f32);
+  let mut z = Tensor::shape_elem(n, 0f32);
+  println!("Default sequential...");
+  timeit!({
+    z = TensorOwned{shape: x.shape,
+                    strides: x.strides,
+                    data: x.view()
+                           .iter()
+                           .zip(y.view().iter())
+                           .map(|(x, y)| x * y)
+                           .collect()}
+  });
+  println!("{:?}", z[0]);
+  z = Tensor::shape_elem(n, 0f32);
+  println!("Opencl parallel...");
+  timeit!({z = x.view() * y.view()});
+  println!("{:?}", z[0]);
 }
